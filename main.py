6568707e7daf799c935eab0b228144593fb164ac
Rain <s.rain@yahoo.com.tw>
2020-11-20 14:15:06 +0800

    enhanse notification

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 import threading
+from datetime import datetime
+
+import os
+
+
 
 URL = 'https://store.sony.com.tw/product/show/ff808081748fa5060174908e4ff000a8'
 frequency = 2500
@@ -25,17 +30,42 @@ def _beep():
   sys.stdout.write('\a')
   sys.stdout.flush()
   
+def _sendNotification():
+  os.system("""
+    osascript -e 'display notification "{}" with title "{}"'
+  """.format('快買喔！！', 'PS5 有貨啦！！！'))
+
+
+def _recordStockStatus(status):
+  f = open("stocklog", "a")
+  record = status + '\n'
+  f.write(record)
+  f.close()
+
+def _getNowDate():
+  now = datetime.now()
+  dateTime = now.strftime("%m/%d/%Y, %H:%M:%S")
+  return dateTime
 
 def _checkStock():
+  status = ''
   searchResult = _getHTML('缺貨中...')
+  dateTime = _getNowDate()
 
   if not searchResult:
-    print '有現貨！！！！！！！！！'
-    print searchResult
+    status = ' - 有現貨！！！！！！！！！'
     _beep()
+    _sendNotification()
+    _recordStockStatus(dateTime + status)
+
   else :
-    print '缺貨中...'
-    
+    status = ' - 缺貨中...'
+
+  record = dateTime + status
+  print record
+
+def _main():
+  setInterval(_checkStock, 3)
+
 if __name__ == '__main__':
-  
-  setInterval(_checkStock, 5)
+  _main()
